953fbc484bab6d6966178498f9fcd6c435860b2d
David Scherer-ODell <dscherer21@gmail.com>
2018-01-31 01:41:29 -0600

    added a background, header, music file, created an array in JS,added game instructions

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+var matrixArray = ["MR ANDERSON", "THERE IS NO SPOON", "AGENT SMITH", "MORPHEUS", "THE MATRIX IS ALL AROUND US","TRINITY", "FREE YOUR MIND NEO", "SEIFER", "DIGITAL PIMP HARD AT WORK"]
+console.log(matrixArray);
+
+window.onload = function() {
+    document.getElementById("myAudio").play();
+}

c0d310f17f3855c31782f3de80f4fef56ff2f9ee
David Scherer-ODell <dscherer21@gmail.com>
2018-01-31 23:51:20 -0600

    added in game field html, styles, and JS. Still a few bugs to work out.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,131 @@
-var matrixArray = ["MR ANDERSON", "THERE IS NO SPOON", "AGENT SMITH", "MORPHEUS", "THE MATRIX IS ALL AROUND US","TRINITY", "FREE YOUR MIND NEO", "SEIFER", "DIGITAL PIMP HARD AT WORK"];
-console.log(matrixArray);
-
 window.onload = function() {
     document.getElementById("myAudio").play();
 }
 
-//game starts on window load
+(function () {
+    "use strict";
+    var availableLetters, words, guessInput, guess, guessButton, lettersGuessed, lettersMatched, output, man, letters, lives, currentWord, numLettersMatched, messages;
 
-//computer generates a random phrase from the matrixArray and displays it as blanks in the game field.
-function getMessage() {
-    return matrixArray[Math.floor(Math.random() * matrixArray.length)];
- }
- console.log(getMessage);
+    function setup() {
+        /* start config options */
+        availableLetters = "abcdefghijklmnopqrstuvwxyz ";
+        lives = 8;
+        words = ["morpheus", "trinity", "seifer", "tank", "dozer", "mouse", "mr anderson", "agent smith", "the oracle", "there is no spoon", "free your mind neo", "digital pimp hard at work", "neo is the one"];
+        messages = {
+            win: 'You win! You must be The One!',
+            lose: 'Game over! The Agents got to you!',
+            guessed: ' already guessed, please try again...',
+            validLetter: 'Please enter a letter from A-Z'
+        };
+        /* end config options */
 
-//Whenever the user types a letter, the computer checks to see if it is the phrase and changes the blanks in the phrase when it does.
+        lettersGuessed = lettersMatched = '';
+        numLettersMatched = 0;
 
-//whenever the user guesses a letter that is not in the word, display it in the guesses field
+        /* choose a word */
+        currentWord = words[Math.floor(Math.random() * words.length)];
 
-// after 8 wrong guess the user adds a point losses and the game resets
+        /* make #man and #output blank, create vars for later access */
+        output = document.getElementById("output");
+        man = document.getElementById("man");
+        guessInput = document.getElementById("letter");
 
-//if the user guesses all the letters in the phrase the player adds a point to wins and the game resets
+        man.innerHTML = 'You have ' + lives + ' lives remaining';
+        output.innerHTML = '';
+
+        document.getElementById("letter").value = '';
+
+        /* make sure guess button is enabled */
+        guessButton = document.getElementById("guess");
+        guessInput.style.display = 'inline';
+        guessButton.style.display = 'inline';
+
+        /* set up display of letters in current word */
+        letters = document.getElementById("letters");
+        letters.innerHTML = '<li class="current-word">Current word:</li>';
+
+        var letter, i;
+        for (i = 0; i < currentWord.length; i++) {
+            letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
+            letters.insertAdjacentHTML('beforeend', letter);
+        }
+    }
+
+    function gameOver(win) {
+        if (win) {
+            output.innerHTML = messages.win;
+            output.classList.add('win');
+        } else {
+            output.innerHTML = messages.lose;
+            output.classList.add('error');
+        }
+
+        guessInput.style.display = guessButton.style.display = 'none';
+        guessInput.value = '';
+    }
+
+    /* Start game - should ideally check for existing functions attached to window.onload */
+    window.onload = setup();
+
+    /* buttons */
+    document.getElementById("restart").onclick = setup;
+
+    /* reset letter to guess on click */
+    guessInput.onclick = function () {
+        this.value = '';
+    };
+
+    /* main guess function when user clicks #guess */
+    document.getElementById('hangman').onsubmit = function (e) {
+        if (e.preventDefault) e.preventDefault();
+        output.innerHTML = '';
+        output.classList.remove('error', 'warning');
+        guess = guessInput.value;
+
+        /* does guess have a value? if yes continue, if no, error */
+        if (guess) {
+            /* is guess a valid letter? if so carry on, else error */
+            if (availableLetters.indexOf(guess) > -1) {
+                /* has it been guessed (missed or matched) already? if so, abandon & add notice */
+                if ((lettersMatched && lettersMatched.indexOf(guess) > -1) || (lettersGuessed && lettersGuessed.indexOf(guess) > -1)) {
+                    output.innerHTML = '"' + guess.toUpperCase() + '"' + messages.guessed;
+                    output.classList.add("warning");
+                }
+                /* does guess exist in current word? if so, add to letters already matched, if final letter added, game over with win message */
+                else if (currentWord.indexOf(guess) > -1) {
+                    var lettersToShow;
+                    lettersToShow = document.querySelectorAll(".letter" + guess.toUpperCase());
+
+                    for (var i = 0; i < lettersToShow.length; i++) {
+                        lettersToShow[i].classList.add("correct");
+                    }
+
+                    /* check to see if letter appears multiple times */
+                    for (var j = 0; j < currentWord.length; j++) {
+                        if (currentWord.charAt(j) === guess) {
+                            numLettersMatched += 1;
+                        }
+                    }
+
+                    lettersMatched += guess;
+                    if (numLettersMatched === currentWord.length) {
+                        gameOver(true);
+                    }
+                }
+                /* guess doesn't exist in current word and hasn't been guessed before, add to lettersGuessed, reduce lives & update user */
+                else {
+                    lettersGuessed += guess;
+                    lives--;
+                    man.innerHTML = 'You have ' + lives + ' lives remaining';
+                    if (lives === 0) gameOver();
+                }
+            }
+        }
+        /* no letter entered, error */
+        else {
+            output.classList.add('error');
+            output.innerHTML = messages.validLetter;
+        }
+        return false;
+    };
+}());
